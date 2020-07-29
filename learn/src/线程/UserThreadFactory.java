a7167ec4e738ab1ac7fafb636c06dd3426d289b1
pushaosen <pushaosen@thunisoft.com>
2020-07-29 11:07:14 +0800

    添加默认的线程池的实现

---
+++ learn/src/线程/UserThreadFactory.java
@@ -0,0 +1,38 @@
+package 线程;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 自定义线程池
+ * @author lenovo
+ *
+ */
+public class UserThreadFactory implements ThreadFactory {
+	
+	private String namePrefix;
+	private final AtomicInteger nextId = new AtomicInteger();
+
+	// 定义线程组的名称
+	UserThreadFactory(String name){
+		namePrefix = "UserThreadFactory's" + name + "-worker-";
+	}
+	
+	@Override
+	public Thread newThread(Runnable task) {
+		String name = namePrefix + nextId.getAndIncrement();
+		Thread thread = new Thread(null, task, name, 0);
+		System.out.println(thread.getName());
+		return thread;
+	}
+
+}
+
+class Task implements Runnable {
+	private final AtomicLong count = new AtomicLong(0L);
+	@Override
+	public void run() {
+		System.out.println("running_" + count.getAndIncrement());
+	}
+}
