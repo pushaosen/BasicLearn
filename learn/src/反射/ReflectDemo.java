8ad474ab112f560e481e4e93dffb6f1d735c9d71
pushaosen <pushaosen@thunisoft.com>
2020-09-01 17:00:51 +0800

    添加反射测试类

--- learn/src/反射/ReflectDemo.java
+++ learn/src/反射/ReflectDemo.java
@@ -1,4 +1,4 @@
-package 力扣算法.动态规划章节算法思想;
+package 反射;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -6,7 +6,7 @@ import java.lang.reflect.Method;
 public class ReflectDemo {
 	
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-		Class<?> clazz = Class.forName("力扣算法.动态规划章节算法思想.ReflectDemo");
+		Class<?> clazz = Class.forName("反射.ReflectDemo");
 		Object instance = clazz.getConstructor().newInstance();
 		for(Method method :clazz.getMethods()) {
             if ("fun".equals(method.getName())) {
